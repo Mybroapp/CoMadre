edfa98cebba90991f58811a292520f35e0efc3e4
Mybroapp <juanantoniofulco@gmail.com>
2025-07-29 22:59:36 -0400

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,6 @@
-// lib/supabase.ts
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://TU-PROYECTO.supabase.co'
-const supabaseAnonKey = 'TU-LLAVE-ANON'
+const supabaseUrl = 'https://nzsceyrenzsfatxgsfsf.supabase.co'
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im56c2NleXJlbnpzZmF0eGdzZnNmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTM4MzY1NDQsImV4cCI6MjA2OTQxMjU0NH0.QXndJAiHoIL4aGfgcVsCs46xclfOSwG5NqcSfPZzuzY'
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
